1205b58f853d12a89c5010aa94de684a320d1846
Filippo DallOlio <fdallolio@brocku.ca>
2024-03-28 00:07:04 -0400

    .

--- R/as_arrow_ptype.R
+++ R/as_arrow_ptype.R
@@ -6,47 +6,47 @@ NULL
 #'
 #' @rdname as_arrow_ptype
 #' @export
-as_arrow_ptype <- function(x, .fn = NULL, tbl = NULL){
+as_arrow_ptype <- function(x, .fn = NULL, tbl_name = NULL){
   UseMethod("as_arrow_ptype")
 }
 #' @rdname as_arrow_ptype
 #' @export
-as_arrow_ptype.Schema <- function(x, .fn = NULL, tbl = NULL){
+as_arrow_ptype.Schema <- function(x, .fn = NULL, tbl_name = NULL){
   f <- function(x) x$code()
   x <- lapply(as_types_list(x, .fn = .fn), f)
-  arrow_ptype(!!!x, tbl = tbl)
+  arrow_ptype(!!!x, tbl_name = tbl_name)
 }
 #' @rdname as_arrow_ptype
 #' @export
-as_arrow_ptype.data.frame <- function(x, .fn = NULL, tbl = NULL){
+as_arrow_ptype.data.frame <- function(x, .fn = NULL, tbl_name = NULL){
   x0 <- schema(x)
   f <- function(x) x$code()
   x <- lapply(as_types_list(x0, .fn = .fn), f)
-  arrow_ptype(!!!x, tbl = tbl)
+  arrow_ptype(!!!x, tbl_name = tbl_name)
 }
 #' @rdname as_arrow_ptype
 #' @export
-as_arrow_ptype.FileSystemDataset <- function(x, .fn = NULL, tbl = NULL){
+as_arrow_ptype.FileSystemDataset <- function(x, .fn = NULL, tbl_name = NULL){
   x0 <- schema(x)
   f <- function(x) x$code()
   x <- lapply(as_types_list(x0, .fn = .fn), f)
-  arrow_ptype(!!!x, tbl = tbl)
+  arrow_ptype(!!!x, tbl_name = tbl_name)
 }
 #' @rdname as_arrow_ptype
 #' @export
-as_arrow_ptype.Table <- function(x, .fn = NULL, tbl = NULL){
+as_arrow_ptype.Table <- function(x, .fn = NULL, tbl_name = NULL){
   x0 <- schema(x)
   f <- function(x) x$code()
   x <- lapply(as_types_list(x0, .fn = .fn), f)
-  arrow_ptype(!!!x, tbl = tbl)
+  arrow_ptype(!!!x, tbl_name = tbl_name)
 }
 #' @rdname as_arrow_ptype
 #' @export
-as_arrow_ptype.StructArray <- function(x, .fn = NULL, tbl = NULL){
+as_arrow_ptype.StructArray <- function(x, .fn = NULL, tbl_name = NULL){
   x0 <- schema(x)
   f <- function(x) x$code()
   x <- lapply(as_types_list(x0, .fn = .fn), f)
-  arrow_ptype(!!!x, tbl = tbl)
+  arrow_ptype(!!!x, tbl_name = tbl_name)
 }
 
 
