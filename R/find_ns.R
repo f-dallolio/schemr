27b68ab9c8e110bdc214b287e809bf661879e3d2
Filippo DallOlio <fdallolio@brocku.ca>
2024-04-06 13:45:58 -0400

    .

--- R/find_ns.R
+++ R/find_ns.R
@@ -1,24 +1,33 @@
-#
-# .auto_ns <- function(x, full_out = TRUE){
-#   check_call(x)
-#   ns <- call_ns(x)
-#   if(is.null(ns)){
-#     fn <- try(get(call_name(x)))
-#     if(is_function(fn)){
-#       ns <- ns_env_name(fn)
-#     } else {
-#       ns <- NULL
-#     }
-#   }
-#   if(full_out){
-#     call2(call_name(x), !!!call_args(x), .ns = ns)
-#   } else {
-#     ns
-#   }
-# }
-# auto_ns <- function(..., ns_only = FALSE){
-#   sapply(list2(...), .auto_ns)
-# }
-#
-# x <- auto_ns(expr(int32()))
-# objs_as_call(arroe::int32)
+#' Find Namespaces
+#'
+#' @param x .
+#' @param full_out .
+#'
+#'
+#' @name find_ns
+NULL
+#'
+#' @rdname find_ns
+#' @export
+.auto_ns <- function(x, full_out = TRUE){
+  check_call(x)
+  ns <- call_ns(x)
+  if(is.null(ns)){
+    fn <- try(get(call_name(x)))
+    if(is_function(fn)){
+      ns <- ns_env_name(fn)
+    } else {
+      ns <- NULL
+    }
+  }
+  if(full_out){
+    call2(call_name(x), !!!call_args(x), .ns = ns)
+  } else {
+    ns
+  }
+}
+#' @rdname find_ns
+#' @export
+auto_ns <- function(..., ns_only = FALSE){
+  sapply(list2(...), .auto_ns)
+}
