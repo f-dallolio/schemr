92d52a59fed77ceea7e78cdbec19a4fc431d0d0a
Filippo DallOlio <fdallolio@brocku.ca>
2024-04-14 19:46:46 -0400

    .

--- R/ns_call.R
+++ R/ns_call.R
@@ -1,8 +1,13 @@
 #' Create Namespaced Call
 #'
+#'@name namespaced-call
+NULL
+#'
+#' @rdname namespaced-call
 #' @export
 ns_call <- function(.ns = NULL, .fn = NULL, ...,
                     .private = FALSE, .strict = FALSE){
+  check_dots_empty()
   lhs <- encall(enexpr(.ns))
   rhs <- encall(enexpr(.fn))
   if(is.null(lhs)) return(rhs)
@@ -12,4 +17,11 @@ ns_call <- function(.ns = NULL, .fn = NULL, ...,
     .op = "::"
   }
   call2(.op, lhs, rhs)
+}
+#'
+#' @export
+namespaced_call <- function(.ns = NULL, .fn = NULL, ...,
+                    .private = FALSE, .strict = FALSE){
+  ns_call <- function(.ns = .ns, .fn = .fn, ...,
+                      .private = .private, .strict = .strict)
 }
